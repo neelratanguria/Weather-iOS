00a05f8ebe4b5333366518060cc6ac97c61569a2
prantae <neelratan@live.com>
2023-06-04 11:18:49 +0530

    Initial Commit

---
+++ Weather-iOS/Weather_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_iOSApp.swift
+//  Weather-iOS
+//
+//  Created by PRANTAE-WS-07  on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
